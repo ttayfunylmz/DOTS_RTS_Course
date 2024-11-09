dcd61ccbef9025e3531ce0d1169b61ab4aa97841
Tayfun Yılmaz <tyfnylmz201974@gmail.com>
2024-11-09 16:48:02 +0300

    v0.1 Basic Movement System w/DOTS

---
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -0,0 +1,35 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+partial struct UnitMoverSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach((
+            RefRW<LocalTransform> localTransform,
+            RefRO<UnitMover> unitMover,
+            RefRW<PhysicsVelocity> physicsVelocity)
+            in SystemAPI.Query<
+                RefRW<LocalTransform>,
+                RefRO<UnitMover>,
+                RefRW<PhysicsVelocity>>())
+        {
+            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
+            float3 moveDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+
+            localTransform.ValueRW.Rotation =
+                math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(moveDirection, math.up()),
+                    unitMover.ValueRO.rotationSpeed * SystemAPI.Time.DeltaTime);
+
+            physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.moveSpeed;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+
+            // localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+        }
+    }
+}
