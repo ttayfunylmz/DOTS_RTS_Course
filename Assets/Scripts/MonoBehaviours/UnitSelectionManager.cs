3d211e6a8fe9aa21c328b145d96cb0b6583abb2e
Tayfun Yılmaz <tyfnylmz201974@gmail.com>
2024-11-12 02:58:31 +0300

    v0.3 Unit Selection Visuals & Single & Multiple Selections

--- Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -1,11 +1,108 @@
+using System;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Physics;
+using Unity.Transforms;
 using UnityEngine;
 
 public class UnitSelectionManager : MonoBehaviour
 {
+    public static UnitSelectionManager Instance { get; private set; }
+
+    public event EventHandler OnSelectionAreaStart;
+    public event EventHandler OnSelectionAreaEnd;    
+
+    private Vector2 selectionStartMousePosition;
+
+    private void Awake() 
+    {
+        Instance = this;    
+    }
+
     private void Update() 
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            selectionStartMousePosition = Input.mousePosition;
+            OnSelectionAreaStart?.Invoke(this, EventArgs.Empty);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 selectionEndMousePosition = Input.mousePosition;
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<Selected>().Build(entityManager);
+
+            NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+
+            for(int i = 0; i < entityArray.Length; ++i)
+            {
+                entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+            }
+            
+            Rect selectionAreaRect = GetSelectionAreaRect();
+
+            float selectionAreaSize = selectionAreaRect.width + selectionAreaRect.height;
+            float multipleSelectionSizeMin = 40f;
+            bool isMultipleSelection = selectionAreaSize > multipleSelectionSizeMin;
+
+            if(isMultipleSelection) // MULTIPLE SELECTION
+            {
+                entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                    .WithAll<LocalTransform, Unit>().WithPresent<Selected>().Build(entityManager);
+
+                entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+                NativeArray<LocalTransform> localTransformArray 
+                    = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+
+                for(int i = 0; i < localTransformArray.Length; ++i)
+                {
+                    LocalTransform unitLocalTransform = localTransformArray[i];
+                    Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+
+                    if(selectionAreaRect.Contains(unitScreenPosition))
+                    {
+                        entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                    } 
+                }
+            }
+            else // SINGLE SELECTION
+            {
+                entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                    .WithAll<PhysicsWorldSingleton>().Build(entityManager);
+
+                PhysicsWorldSingleton physicsWorldSingleton = entityQuery.GetSingleton<PhysicsWorldSingleton>();
+                CollisionWorld collisionWorld = physicsWorldSingleton.CollisionWorld;
+                UnityEngine.Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                int unitsLayer = 6;
+
+                RaycastInput raycastInput = new RaycastInput
+                {
+                    Start = cameraRay.GetPoint(0f),
+                    End = cameraRay.GetPoint(9999f),
+                    Filter = new CollisionFilter
+                    {
+                        BelongsTo = ~0u,
+                        CollidesWith = 1u << unitsLayer,
+                        GroupIndex = 0,
+                    }
+                };
+
+                if(collisionWorld.CastRay(raycastInput, out Unity.Physics.RaycastHit raycastHit))
+                {
+                    if(entityManager.HasComponent<Unit>(raycastHit.Entity)) // HIT A UNIT
+                    {
+                        entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
+                    }
+                }
+            }
+
+            OnSelectionAreaEnd?.Invoke(this, EventArgs.Empty);
+        }
+
         if(Input.GetMouseButtonDown(1))
         {
             Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
@@ -26,5 +123,30 @@ public class UnitSelectionManager : MonoBehaviour
             }
             entityQuery.CopyFromComponentDataArray(unitMoverArray);
         }
+    }
+
+    public Rect GetSelectionAreaRect()
+    {
+        Vector2 selectionEndMousePosition = Input.mousePosition;
+
+        Vector2 lowerLeftCorner = new Vector2
+        (
+            Mathf.Min(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Min(selectionStartMousePosition.y, selectionEndMousePosition.y)
+        );
+
+        Vector2 upperRightCorner = new Vector2
+        (
+            Mathf.Max(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Max(selectionStartMousePosition.y, selectionEndMousePosition.y)
+        );
+
+        return new Rect
+        (
+            lowerLeftCorner.x,
+            lowerLeftCorner.y,
+            upperRightCorner.x - lowerLeftCorner.x,
+            upperRightCorner.y - lowerLeftCorner.y
+        );
     }
 }
